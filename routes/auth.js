5805d9c8aa3c62b5027016c985bafc824f8342d4
Johnny Wong <johnny.wong2324@gmail.com>
2022-04-21 22:05:03 -0400

    done styling landing page and with navs

--- routes/auth.js
+++ routes/auth.js
@@ -93,13 +93,14 @@ router
 router
     .route('/')
     .get(async (req, res) => {
-        if (req.session.user) {
-            const templateData = {
-                authenticated: true
-            }
-            return res.render('templates/index', templateData)
+        const templateData = {
+            isLandingPage: true,
+            authenticated: false
         }
-        else return res.render('templates/index')
+        if (req.session.user) {
+            templateData.authenticated = true
+        }
+        return res.render('templates/index', templateData)
     })
 
 
