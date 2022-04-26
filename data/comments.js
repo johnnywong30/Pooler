4030e8fcb415813b4005c886c908b85d035f7b98
Jordan Wang <jordanwng58@gmail.com>
2022-04-26 14:36:04 -0400

    setup CRUD for comments

--- data/comments.js
+++ data/comments.js
@@ -1,15 +1,17 @@
-// TODO: Write comment data methods here
-/*
-            _     _
-           /@\---/@\
-         ,'         `.
-        |             |
-        <`-----------'>
-       / `. `-----' ,' \
-      /    `-------'    \
-     :  |   _______   |  :
-     |  `.,'       `.,'  |
-    ,`.   \    o    /   ,'.
-   /   `.  `.     ,'  ,'   \
- ^^^^----^^^^-----^^^^----^^^^
-    */
+const { checkId, checkString, checkEmail, checkPassword, checkFirstName, checkLastName, checkPhone, checkVenmo, checkAddress, checkIsDriver } = require("../misc/validate");
+const { users } = require("../config/mongoCollections");
+
+module.exports = {
+    async createComment(userId, description, timestamp) {
+
+    },
+    async getComment(commentId, userId) {
+
+    },
+    async deleteComment(commentId, userId, description, timestamp) {
+
+    },
+    async updateComment(commentId, userId, description, timestamp) {
+
+    }
+}
