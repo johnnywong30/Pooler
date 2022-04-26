304cf2e6afb214f74f86ad90cdc53d7688ac2c68
Jordan Wang <jordanwng58@gmail.com>
2022-04-26 14:57:00 -0400

    fix parameters for all CRUD functions

--- data/comments.js
+++ data/comments.js
@@ -1,17 +1,30 @@
-const { checkId, checkString, checkEmail, checkPassword, checkFirstName, checkLastName, checkPhone, checkVenmo, checkAddress, checkIsDriver } = require("../misc/validate");
+const { checkId, checkString, checkEmail, checkPassword, checkFirstName, checkLastName, checkPhone, checkVenmo, checkAddress, checkIsDriver, checkDateTime } = require("../misc/validate");
 const { users } = require("../config/mongoCollections");
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = {
-    async createComment(userId, description, timestamp) {
+    async createComment(carpoolId, email, details) {
+        let _carpoolId = checkId(carpoolId)
+        let _email = checkEmail(email)
+        let _details = checkString(details)
+        // check if user exists
+        const collection = await users();
+		const account = await collection.findOne({ _id: _userId });
+		if (account !== null) throw `Error: createComment with id ${_userId} does not exist.`;
+        //create comment
+        const newComment = {
+            _id: uuidv4(),
+            from: _userId,
+            details: _details
+        }
+    },
+    async getComment(carpoolId, commentId, email) {
 
     },
-    async getComment(commentId, userId) {
+    async deleteComment(carpoolId, commentId, email) {
 
     },
-    async deleteComment(commentId, userId) {
-
-    },
-    async updateComment(commentId, userId, description, timestamp) {
+    async updateComment(carpoolId, commentId, email, description) {
 
     }
 }
