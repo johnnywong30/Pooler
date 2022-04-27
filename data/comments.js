664aadf27d1b8a1e93f79aa99fad0b0aa4083394
Jordan Wang <jordanwng58@gmail.com>
2022-04-27 11:39:55 -0400

    add deleteComment

--- data/comments.js
+++ data/comments.js
@@ -45,8 +45,34 @@ module.exports = {
         if (!comment) throw `Error: getComment comment ${commentId} does not exist`
         return comment
     },
+    async getAllComments(eventId, poolId) {
+        let _eventId = checkId(eventId)
+        let _poolId = checkId(poolId)
+        const eventCollection = await events()
+        const event = await eventCollection.findOne({_id: _eventId})
+        if (!event) throw `Error: getComment event ${_eventId} does not exist.`
+        const carpool = event.carpools.find(carpool => carpool._id === _poolId)
+        if (!carpool) throw `Error: getComment carpool ${_poolId} does not exist`
+        const comments = carpool.comments
+        if (!comments) throw `Error: getAllComments could not get comments`
+        return comments
+    },
     async deleteComment(eventId, poolId, commentId) {
-
+        let _eventId = checkId(eventId)
+        let _poolId = checkId(poolId)
+        let _commentId = checkId(commentId)
+        const eventCollection = await events()
+        const event = await eventCollection.findOne({_id: _eventId})
+        if (!event) throw `Error: getComment event ${_eventId} does not exist.`
+        const carpool = event.carpools.find(carpool => carpool._id === _poolId)
+        if (!carpool) throw `Error: getComment carpool ${_poolId} does not exist`
+        const comment = carpool.comments.find(comment => comment._id === _commentId)
+        if (!comment) throw `Error: getComment comment ${commentId} does not exist`
+        const updateCarpool = await eventCollection.deleteOne(
+            {_id: _eventId, "carpools._id": _poolId, "carpool.$.comments._id": _commentId}
+        )
+        if (updateCarpool.modifiedCount === 0) throw `Error: could not createComment successfully`
+        return { commentDeleted: true }
     },
     async updateComment(eventId, poolId, commentId, description) {
 
