cdc5337950fceed5f84ab10231d1b47a3f422d92
RolandTumbokon <55549129+RolandTumbokon@users.noreply.github.com>
2022-04-28 15:11:06 -0400

    json files and seed.js complete
    Finished for users and events

--- config/seed.js
+++ config/seed.js
@@ -1,174 +1,59 @@
 const connection = require('./mongoConnection')
 const data = require('../data/')
 const { ObjectId } = require('mongodb')
-const users = data.users
-const events = data.events
+const usersJson = require('./seeds/users.json')
+const eventsJson = require('./seeds/events.json')
+const usersData = data.users;
+const eventsData = data.events;
 
 const setup = async () => {
     const db = await connection.connectToDb()
     await db.dropDatabase()
+
+    const userCollection = await db.collection('users')
+    const eventCollection = await db.collection('events')
     
-    /*          CREATING USERS
-     * createUser(email, password, firstName, lastName, phone, venmo, address, isDriver)
-     */
-    try {
-        await users.createUser(
-            "email@gmail.com", 
-            "password1",
-            "Adam", 
-            "Smith", 
-            "2315232251", 
-            "a.smith", 
-            {
-                address: "406 Jefferson St",
-                city: "Hoboken",
-                state: "NJ",
-                zipcode: "07030"
-            }, 
-            true)
-    } catch (e) {
-        console.log(e);
+    // Seeding Users
+    // createUser(email, password, firstName, lastName, phone, venmo, address, isDriver)
+
+    for (user of usersJson.users) {
+        console.log(`Seeding user ${user.firstName} ${user.lastName}`)
+        try {
+            await usersData.createUser(
+                user.email,
+                user.password,
+                user.firstName,
+                user.lastName,
+                user.phone,
+                user.venmo,
+                user.address,
+                user.driver
+            )
+        } catch (e) {
+            console.log(e);
+        }
     }
 
-    try {
-        await users.createUser(
-            "jwang203@stevens.edu", 
-            "notmypass",
-            "Jordan", 
-            "Wang", 
-            "9192341667", 
-            "big-wang-69", 
-            {
-                address: "422 Monroe St",
-                city: "Hoboken",
-                state: "NJ",
-                zipcode: "07030"
-            }, 
-            true)
-    } catch (e) {
-        console.log(e);
+    // Seeding events 
+    // createEvent(_name, _date, _startTime, _host, _description, _capacity, _destination, _private, _password = '')
+    for (event_ of eventsJson.events) {
+        console.log(`Seeding event ${event_.name}`)
+        try {
+            await eventsData.createEvent(
+                event_.name,
+                event_.date,
+                event_.startTime,
+                event_.host,
+                event_.description,
+                event_.capacity,
+                event_.destination,
+                event_.private,
+                event_.password
+            )
+        } catch (e) {
+            console.log(e);
+        }
     }
-
-    try {
-        await users.createUser(
-            "zuo12@stevens.edu", 
-            "ilovedogs12",
-            "Sophia", 
-            "Zuo", 
-            "9127341337", 
-            "sophia.zuo", 
-            {
-                address: "500 Adams St",
-                city: "Hoboken",
-                state: "NJ",
-                zipcode: "07030"
-            }, 
-            false)
-    } catch (e) {
-        console.log(e);
-    }
-
-    try {
-        await users.createUser(
-            "wong@stevens.edu", 
-            "hunter13",
-            "Johnny", 
-            "Wong", 
-            "9074140327", 
-            "stevensfrisbeeclub", 
-            {
-                address: "612 Washington St",
-                city: "Hoboken",
-                state: "NJ",
-                zipcode: "07030"
-            }, 
-            false)
-    } catch (e) {
-        console.log(e);
-    }
-
-    try {
-        await users.createUser(
-            "nsoriano11@stevens.edu", 
-            "takenaf",
-            "Nicholas", 
-            "Soriano", 
-            "5631120300", 
-            "rubysnailsalon", 
-            {
-                address: "333 River St",
-                city: "Hoboken",
-                state: "NJ",
-                zipcode: "07030"
-            }, 
-            false)
-    } catch (e) {
-        console.log(e);
-    }
-
-    try {
-        await users.createUser(
-            "rtumboko@stevens.edu", 
-            "password1",
-            "Roland", 
-            "Tumbokon", 
-            "9084995818", 
-            "rolandtumbokon", 
-            {
-                address: "123 N Union Ave",
-                city: "Cranford",
-                state: "NJ",
-                zipcode: "07016"
-            }, 
-            false)
-    } catch (e) {
-        console.log(e);
-    }
-
-    /*                  CREATING EVENTS
-     * createEvent(_name, _date, _startTime, _host, _description, _capacity, _destination, _private, _password = '')
-    */ 
-
-    try {
-        await events.createEvent(
-            "Jordan Wang's Dance Party", 
-            "04/01/2022",
-            "13:30", 
-            "jwang203@stevens.edu", 
-            "Everyone is invited to dance to WAP and Twice songs with me!", 
-            40,
-            {
-                address: "422 Monroe St",
-                city: "Hoboken",
-                state: "NJ",
-                zipcode: "07030"
-            }, 
-            true,
-            "jaanav")
-    } catch (e) {
-        console.log(e);
-    }
-
-    try {
-        await events.createEvent(
-            "Patrick Hill's Dance Recital", 
-            "July 1",
-            "02:05", 
-            "zuo12@stevens.edu", 
-            "Dancing in the Hills! Patrick Hill and his lovely wife are teaching us the roomba. Be there or be square!", 
-            12,
-            {
-                address: "EAS 320",
-                city: "Hoboken",
-                state: "NJ",
-                zipcode: "07030"
-            }, 
-            false,
-            "")
-    } catch (e) {
-        console.log(e);
-    }
-
 
     console.log('Done seeding database');
 
