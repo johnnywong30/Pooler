6b402a1aa98034557c7a3cd8a6cb02f167f451cf
Jordan Wang <jordanwng58@gmail.com>
2022-05-08 14:07:26 -0400

    fix events routes

--- routes/events.js
+++ routes/events.js
@@ -16,7 +16,7 @@ router
                 authenticated: true,
                 layout: "custom",
             };
-            return res.render("templates/eventlist", templateData);
+            return res.status(200).render("templates/eventlist", templateData);
         } else {
             return res.redirect("/");
         }
@@ -26,7 +26,15 @@ router
     .route('/getUser')
     .get(async (req, res) => {
         if (req.session.user) {
-            return res.json(req.session.user.email);
+            // validate
+            try {
+                req.session.user.email = checkEmail(xss(req.session.user.email))
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(400).json({ errorMsg: e }).end();
+            }
+            return res.status(200).json(req.session.user.email);
         }
         else {
             return res.redirect('/')
@@ -38,10 +46,27 @@ router
     .route('/view/:id') // specific event page with details
     .get(async (req, res) => {
         if (req.session.user) {
+            // validate
             try {
-                const id = checkId(xss(req.params.id))
-                const userData = await users.getUser(xss(req.session.user.email))
-                const eventData = await events.getEvent(id)
+                req.session.user.email = checkEmail(xss(req.session.user.email))
+                req.params.id = checkId(xss(req.params.id))
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(400).json({ errorMsg: e }).end();
+            }
+            // check if user and event exists
+            let userData, eventData
+            const id = req.params.id
+            try {
+                userData = await users.getUser(xss(req.session.user.email))
+                eventData = await events.getEvent(id)
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(404).json({ errorMsg: e }).end();
+            }
+            try {
                 const { _id, name, date, startTime, host, description } = eventData
                 const { capacity, carpools, destination, private, password } = eventData
                 // if event has password, see if it is correct
@@ -94,7 +119,7 @@ router
                     authenticated: true
                 }
                 // render the event page
-                return res.render("templates/event", templateData);
+                return res.status(200).render("templates/event", templateData);
             } catch (e) {
                 console.log(e)
                 return res.status(400).json({ error: "Invalid password" });
@@ -123,9 +148,9 @@ router.route("/list").get(async (req, res) => {
                     private: event.private,
                 };
             });
-            return res.json(renderedEvents);
+            return res.status(200).json(renderedEvents);
         } catch (e) {
-            return res.status(500).json({ error: e });
+            return res.status(404).json({ error: e });
         }
     } else {
         return res.redirect("/");
@@ -136,22 +161,31 @@ router
     .route("/createEvent")
     .post(async (req, res) => {
         if (req.session.user) {
-            const host = xss(req.session.user.email);
+            const { name, date, startTime, description, capacity, private, password, destination } = req.body;
+            let host, _name, _date, _startTime, _host, _description, _capacity, _private, _pass, _destination
+            // validate
             try {
-                const { name, date, startTime, description, capacity, private, password, destination } = req.body;
-                const _name = checkString(xss(name));
-                const _date = checkDate(xss(date));
-                const _startTime = checkTime(xss(startTime));
-                const _host = checkEmail(host); // should be in session
-                const _description = checkString(xss(description));
+                host = xss(req.session.user.email);
+                _name = checkString(xss(name));
+                 _date = checkDate(xss(date));
+                _startTime = checkTime(xss(startTime));
+                _host = checkEmail(host); // should be in session
+                _description = checkString(xss(description));
                 const cap = typeof capacity === 'number' ? capacity : xss(capacity)
-                const _capacity = checkCapacity(cap); // is a number
+                _capacity = checkCapacity(cap); // is a number
                 const boolVal = typeof private === 'boolean' ? private : xss(private)
-                const _private = checkBool(boolVal); // is a bool
-                const _pass = _private ? checkPassword(xss(password)) : null;
-                const _destination = checkAddress(destination);
+                _private = checkBool(boolVal); // is a bool
+                _pass = _private ? checkPassword(xss(password)) : null;
+                _destination = checkAddress(destination);
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(400).json({ errorMsg: e }).end();
+            }
+            // create event
+            try {
                 const event = await events.createEvent(_name, _date, _startTime, _host, _description, _capacity, _destination, _private, _pass);
-                return res.json({ success: true, eventId: event.eventId }).end();
+                return res.status(200).json({ success: true, eventId: event.eventId }).end();
             } catch (e) {
                 console.log(e);
                 res.statusMessage = e;
@@ -164,12 +198,21 @@ router
 
 router.get("/:id", async (req, res) => {
     if (req.session.user) {
+        // validate
         try {
-            const id = checkId(xss(req.params.id));
+            req.params.id = checkId(xss(req.params.id))
+        } catch (e) {
+            console.log(e);
+            res.statusMessage = e;
+            return res.status(400).json({ errorMsg: e }).end();
+        }
+        // check if event exists
+        try {
+            const id = req.params.id;
             const event = await events.getEvent(id);
             return res.status(200).json({ event });
         } catch (e) {
-            return res.status(400).json({ error: e });
+            return res.status(404).json({ error: e });
         }
     } else {
         return res.redirect('/')
@@ -180,12 +223,21 @@ router
     .route('/getHost/:id')
     .get(async (req, res) => {
         if (req.session.user) {
+             // validate
             try {
-                const id = checkId(xss(req.params.id))
+                req.params.id = checkId(xss(req.params.id))
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(400).json({ errorMsg: e }).end();
+            }
+            // check if event exists
+            try {
+                const id = req.params.id
                 const event = await events.getEvent(id)
                 return res.status(200).json(event.host);
             } catch (e) {
-                return res.status(400).json({ error: e });
+                return res.status(404).json({ error: e });
             }
         } else {
             return res.redirect('/')
@@ -195,12 +247,21 @@ router
 router
     .delete('/:id', async (req, res) => {
         if (req.session.user) {
+            // validate
             try {
-                const id = checkId(xss(req.params.id))
+                req.params.id = checkId(xss(req.params.id))
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(400).json({ errorMsg: e }).end();
+            }
+            // delete event
+            try {
+                const id = req.params.id
                 await events.deleteEvent(id)
                 return res.status(200).json({ eventId: id, deleted: true });
             } catch (e) {
-                return res.status(400).json({ error: e });
+                return res.status(404).json({ error: e });
             }
         } else {
             return res.redirect('/')
@@ -210,9 +271,19 @@ router
 router.post("/validateEvent/:id", async (req, res) => {
     if (req.session.user) {
         let auth = {};
+        // validate
         try {
-            const id = checkId(xss(req.params.id));
-            const password = checkPassword(xss(req.body.password));
+            req.params.id = checkId(xss(req.params.id))
+            req.body.password = checkPassword(xss(req.body.password))
+        } catch (e) {
+            console.log(e);
+            res.statusMessage = e;
+            return res.status(400).json({ errorMsg: e }).end();
+        }
+        //validate event
+        try {
+            const id = req.params.id;
+            const password = req.body.password;
             auth = await events.validateEvent(id, password);
         } catch (e) {
             return res.status(400).json({ error: e });
@@ -227,42 +298,54 @@ router.post("/validateEvent/:id", async (req, res) => {
 })
 router
     .post('/updateEvent/:id', async (req, res) => {
-        const { name, date, startTime, description, destination } = req.body;
-        try {
-            const id = checkId(xss(req.params.id))
-            let _name = checkString(xss(name), "event name")
-            let _date = checkDate(xss(date))
-            let _startTime = checkTime(xss(startTime))
-            let _description = checkString(xss(description))
-            // all addresses should be cleaned by middleware
-            let _destination = checkAddress(destination)
-
-            const currentEvent = await events.getEvent(id);
-
-            if (currentEvent.name !== _name) {
-                const updateName = await events.updateName(id, _name)
+        if (req.session.user) {
+            const { name, date, startTime, description, destination } = req.body;
+            // validate
+            let id, _name, _date, _startTime, _description, _destination 
+            try {
+                id = checkId(xss(req.params.id))
+                _name = checkString(xss(name), "event name")
+                _date = checkDate(xss(date))
+                _startTime = checkTime(xss(startTime))
+                _description = checkString(xss(description))
+                // all addresses should be cleaned by middleware
+                _destination = checkAddress(destination)
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(400).json({ errorMsg: e }).end();
             }
-
-
-            if (currentEvent.date != _date) {
-                const updateDate = await events.updateDate(id, _date)
+            // check if event exists
+            let currentEvent
+            try {
+                currentEvent = await events.getEvent(id);
+            } catch (e) {
+                console.log(e);
+                res.statusMessage = e;
+                return res.status(404).json({ errorMsg: e }).end();
             }
+            // update event
+            try {
+                if (currentEvent.name !== _name) {
+                    const updateName = await events.updateName(id, _name)
+                }
+                if (currentEvent.date != _date) {
+                    const updateDate = await events.updateDate(id, _date)
+                }
+                if (currentEvent.startTime !== _startTime) {
+                    const updateTime = await events.updateStartTime(id, _startTime)
+                }
+                if (currentEvent.description !== _description) {
+                    const updateDescription = await events.updateDescription(id, _description)
+                }
 
-            if (currentEvent.startTime !== _startTime) {
-                const updateTime = await events.updateStartTime(id, _startTime)
+                const updateDestination = await events.updateDestination(id, _destination)
+                const updatedEvent = await events.getEvent(id);
+
+                return res.status(200).json({ event: updatedEvent, updated: true })
+            } catch (e) {
+                return res.status(400).json({ error: e })
             }
-
-            if (currentEvent.description !== _description) {
-                const updateDescription = await events.updateDescription(id, _description)
-            }
-
-            const updateDestination = await events.updateDestination(id, _destination)
-
-            const updatedEvent = await events.getEvent(id);
-
-            return res.status(200).json({ event: updatedEvent, updated: true })
-        } catch (e) {
-            return res.status(400).json({ error: e })
         }
 
     })
